ba2ee0139db644fc443e9956434dffca96d6fb11
juanmaorta <juanma.ort@gmail.com>
2019-01-28 09:50:22 +0100

    fixup! Adding redux -> delete card (no confirmation)

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 it('renders without crashing', () => {
+  const store = {
+    getState: jest.fn,
+    subscribe: jest.fn,
+    dispatch: jest.fn
+  }
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<App store={store} />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
